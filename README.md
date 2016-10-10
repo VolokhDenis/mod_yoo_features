66a09c7d7475b4d216ec0cc4b2ae2ecfe24f457e
Denis Volokh <davoloh@gmail.com>
2016-10-10 22:09:16 +0300

    Add text area for edit

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Joomla! 3+ module for features Features module for Joomla 3+ version 1.1
 
 Features module for Joomla 3+
-version 1.0
+version 1.1
 
 Main module parts visible in UI: 
 
 1. Feature Intro text
+	a. Added to default template
 2. Feature Intro Picture
 3. Feature description text
-4. Feature description picture  - ? Not realised
-5. Background color for feature back - ? Not realised
+	a. Added INtro text to description part
+4. Feature card flip setup  - added
+5. Add URL onclick 
--- tmpl/default.php
+++ tmpl/default.php
@@ -11,6 +11,7 @@ defined('_JEXEC') or die;
 
 $displayMode = $params->get('display_mode', 0);
 $title = $params->get('title');
+$flip_type = $params->get('flip_type');
 ?>
 
 		<?php if( isset($title)){?>
@@ -21,19 +22,21 @@ $title = $params->get('title');
 
 
 
-	<div class="serv_board"> <!--possible to improve as chooseble row type fluid/non fluid-->
+	<div class="serv_board"> 
 	    <?php for ($i = 0; $i <= $cards-1; $i++){
 					$num=$i+1;
 					$yoo_main_board.= '<div id="serv_card_'.$num.'" class="yoo-feature col-lg-'.round(12/$cards).' col-md-'.round(12/$cards).' '.$class.'">';
-						$yoo_main_board.='<div class="flipcard h">';//в будущем дать выбор перевррота h или v
+						$yoo_main_board.='<div class="flipcard '.$flip_type.'">';//в будущем дать выбор перевррота h или v
 							//$yoo_main_board.='<div class="screen"></div><!--Экран накрывающий картинку после наведения-->';
 							$yoo_main_board.='<div class="serv_card_front">';
+								$yoo_main_board.='<div class="IntroText"><h2>'.$data[$i]['IntroText'].'</h2></div>';
 								$yoo_main_board.='<img class="front_image" src="'.$data[$i]['IntroPic'].'"/>';
 							$yoo_main_board.='</div>';
 
 							$yoo_main_board.='<div class="serv_card_back">';
 								$yoo_main_board.='<div class="info_table">';
-									$yoo_main_board.='<a>'.$data[$i]['DescText'].'</a>';
+									$yoo_main_board.='<h3>'.$data[$i]['IntroText'].'</h3>';
+									$yoo_main_board.='<p>'.$data[$i]['DescText'].'</p>';
 								$yoo_main_board.='</div><!--Info_table close-->';
 								if( isset($data[$i]['DescMore'])){
 									$yoo_main_board.='<button class="more_button"><a>more</a></button>';//ADD more button
