0b256bea7522cc25a8d25a0dd23cb584be4231fb
Denis Volokh <davoloh@gmail.com>
2016-10-22 23:22:18 +0300

    Added sizing of cards

--- tmpl/default.php
+++ tmpl/default.php
@@ -12,6 +12,8 @@ defined('_JEXEC') or die;
 $displayMode = $params->get('display_mode', 0);
 $title = $params->get('title');
 $flip_type = $params->get('flip_type');
+$card_size_w = $params->get('card_size_w');
+$card_size_h = $params->get('card_size_h');
 ?>
 
 		<?php if( isset($title)){?>
@@ -26,7 +28,7 @@ $flip_type = $params->get('flip_type');
 	    <?php for ($i = 0; $i <= $cards-1; $i++){
 					$num=$i+1;
 					$yoo_main_board.= '<div id="serv_card_'.$num.'" class="yoo-feature col-lg-'.round(12/$cards).' col-md-'.round(12/$cards).' '.$class.'">';
-						$yoo_main_board.='<div class="flipcard '.$flip_type.'">';//в будущем дать выбор перевррота h или v
+						$yoo_main_board.='<div style="width: '.$card_size_w.'px; height: '.$card_size_h.'px;" class="flipcard '.$flip_type.'">';//в будущем дать выбор перевррота h или v
 							//$yoo_main_board.='<div class="screen"></div><!--Экран накрывающий картинку после наведения-->';
 							$yoo_main_board.='<div class="serv_card_front">';
 								$yoo_main_board.='<div class="IntroText"><h2>'.$data[$i]['IntroText'].'</h2></div>';
